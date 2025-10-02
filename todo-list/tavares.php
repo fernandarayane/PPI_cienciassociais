99b39477f7591deca6c41b6509a40ad28f8fb324
fernandarayane <fernanda.rayane@escolar.ifrn.edu.br>
2025-10-02 16:15:12 -0300

    funcao criar e listar feito

--- todo-list/tavares.php
+++ todo-list/tavares.php
@@ -5,16 +5,36 @@ $localhost = 'localhost';
 $usuario = 'root';
 $senha = '';
 $database = 'todo_list';
-$conn = mysqli_connect(hostname: $localhost, username: $usuario, database: $database);
+$conn = new mysqli(hostname: $localhost, username: $usuario, database: $database);
 
 if($conn -> connect_error){
     die('deu erro ao tentar conectar '. mysqli_connect_error());
 }
 
+# criacao tarefa
 
-$tarefas = []
- 
-?>
+if (isset ($_POST['descricao']) && !empty(trim ($_POST['descricao']))) {
+ $descricao = $conn -> real_escape_string(string: $_POST ['descricao']);
+ $sqlcreate = " INSERT INTO tarefas (descricao) VALUES ('$descricao')";
+
+   if($conn -> query($sqlcreate) === TRUE) { 
+   header (header: "location: tavares.php");
+
+    }
+}
+
+# listar tarefas
+$tarefas = [];
+
+$sqlselect = "SELECT * FROM tarefas ORDER BY data_criacao DESC";
+$result = $conn -> query(query: $sqlselect);
+
+if($result-> num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $tarefas[] = $row;
+}
+}
+?> 
 
 <!DOCTYPE html>
 <html lang="en">
@@ -26,14 +46,21 @@ $tarefas = []
 <body>
 
     <h1>TO-DO List</h1>
-    <form action="todo-list2.php" method="POST">
+    <form action="tavares.php" method="POST">
         <input type="text" placeholder="Descrição da sua tarefa" name="descricao"/>
         <button type="submit">Adicionar</button> 
     </form>
 
     <h2>Suas tarefas</h2>
     <?php if(!empty($tarefas)):?>
-    <h3>Suas tarefas</h3>
+        <ul>
+             <?php foreach($tarefas as $tarefa): ?>
+             <li>
+                <?php echo $tarefa['descricao']?>
+             </li>
+             <?php endforeach?>; 
+
+        </ul>
     <?php else:?>
     <h3>Não tem tarefas</h3>
     <?php endif;?>
